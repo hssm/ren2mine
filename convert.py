368e919ec8b2f6d1360ecbc948c2567b4687232e
Houssam Salem <houssam.salem.au@gmail.com>
2017-10-22 22:45:25 +1100

    Do an entire game dir at once

--- convert.py
+++ convert.py
@@ -1,12 +1,16 @@
 import re
+import os
+from pathlib import Path
 
-input = "/home/hssm/.local/share/Steam/steamapps/common/Sound of Drop - fall into poison -/game/scenario/0-01.rpy"
-output = "/home/hssm/out.html"
-
-lineRegex = re.compile(r'\s*(.+) (".+")')
+lineRegex = re.compile(r'\s*(.*)\s*(".+")')
 rubyRegex = re.compile(r'{rb}(.+?){/rb}\s*?{rt}(.+?){/rt}')
 
-def cleanFile(script, out):
+def cleanFile(infile, outfile):
+    if not os.path.exists(os.path.dirname(outfile)):
+        os.makedirs(os.path.dirname(outfile))
+    out = open(outfile, "w+")
+    script = open(infile)
+    names = set()
     out.write("""
 <!DOCTYPE html>
 <html>
@@ -14,7 +18,7 @@ def cleanFile(script, out):
 <style>
 body { 
     background-color: #000;
-    color: #fff;
+    color: #a1caff;
     font-size: 18px;
 }
 </style>
@@ -28,28 +32,39 @@ body {
         name = m.group(1)
         line = m.group(2)
 
+        # Skip some non-dialogue lines that slip through.
+        # No guarantee these cover every case
+        if name.startswith("$") or name.startswith("play"):
+            continue
+
         # Replace ruby tags with html versions
         line = rubyRegex.sub("<ruby>\g<1><rt>\g<2></rt></ruby>", line)
 
         if name is not None:
-            out.writelines("<div>{} {}</div>".format(name, line))
+            names.add(name)
+            out.writelines("<div>{} {}</div>"
+                           .format(name, line))
         else:
             out.writelines("<div>{}</div>".format(line))
 
-    out.write("""
-</body>
-</html>
-""")
-
-
-
-def getColor(s):
-    h = hash(s)
-    r = (h & 0xFF0000) >> 16
-    g = (h & 0x00FF00) >> 8
-    b = h & 0x0000FF
-    print ("rgb({},{},{})".format(r, g, b))
-
+    out.write("</body>")
+    out.write("</html>")
 
 if __name__ == "__main__":
-    cleanFile(open(input), open(output, "w+"))
+    source_dir = "/home/hssm/.local/share/Steam/steamapps/common/Sound of Drop - fall into poison -/"
+    output_dir = "/home/hssm/mineme/"
+
+    # Last part of path is assumed to be the game name
+    parts = Path(source_dir).parts
+    start_at = len(parts) - 1
+    game = parts[start_at]
+
+    for root, subdirs, files in os.walk(source_dir):
+        subdirs = Path(root).parts[start_at:]
+        for file in files:
+            if file.endswith(".rpy"):
+                current_subdir = "/".join(subdirs)
+                outname = Path(file).stem + ".html"
+                outfile = os.path.join(output_dir, current_subdir, outname)
+                infile = os.path.join(root, file)
+                cleanFile(infile, outfile)
